8891a9a855fcd32d4c534e988a5c472b8b49b307
ShradhaVastrakar <shradhavastrakar@gmail.com>
2023-02-22 00:24:52 +0530

    DAY-1 BROOKSTONE.COM/ Worked on the Landing Page

---
+++ Frontend/index.js
@@ -0,0 +1,56 @@
+const MassagePopup = () => {
+    document.querySelector(".massage-dropdown").classList.remove("hidden");
+}
+
+const Massageleave = () => {
+    document.querySelector(".massage-dropdown").classList.add("hidden");
+}
+
+const WellnessPopup = () =>{
+    document.querySelector(".wellness-dropdown").classList.remove("hidden");
+}
+
+const Wellnessleave = () => {
+    document.querySelector(".wellness-dropdown").classList.add("hidden");
+}
+
+const SleepPopup = () => {
+    document.querySelector(".sleep-dropdown").classList.remove("hidden");
+}
+const Sleepleave = () => {
+    document.querySelector(".sleep-dropdown").classList.add("hidden");
+}
+
+$('#slick1').slick({
+    rows: 1,
+    dots: false,
+    arrows: false,
+    autoplay:true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+$('#featureSlide').slick({
+    rows: 1,
+    dots: false,
+    arrows: false,
+    autoplay:true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+
+
+$('.best-sellers-slider').slick({
+    rows: 1,
+    dots: false,
+    arrows: false,
+    autoplay:true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    slidesToScroll: 3
+});
